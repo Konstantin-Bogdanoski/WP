6e31dbbb1366c68213c109ba56428b2dc25a2286
Konstantin Bogdanoski <konstantin.b@live.com>
2019-11-17 14:58:23 +0100

    - Reformat code

--- wp-2019/src/main/java/ukim/mk/finki/konstantin/bogdanoski/wp/web/MainController.java
+++ wp-2019/src/main/java/ukim/mk/finki/konstantin/bogdanoski/wp/web/MainController.java
@@ -1,11 +1,7 @@
 package ukim.mk.finki.konstantin.bogdanoski.wp.web;
 
-import jdk.nashorn.internal.ir.debug.JSONWriter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import ukim.mk.finki.konstantin.bogdanoski.wp.service.PizzaService;
 
 /**
  * @author Konstantin Bogdanoski (konstantin.b@live.com)
@@ -13,15 +9,4 @@ import ukim.mk.finki.konstantin.bogdanoski.wp.service.PizzaService;
 @Controller
 @AllArgsConstructor
 public class MainController {
-    private PizzaService pizzaService;
-
-    @RequestMapping(value = "/controller", produces = "application/json")
-    @ResponseBody
-    public String pizza() {
-        StringBuilder stringBuilder = new StringBuilder();
-        pizzaService.findAll().stream().forEach(pizza -> {
-            stringBuilder.append(pizza.getName());
-        });
-        return stringBuilder.toString();
-    }
 }
